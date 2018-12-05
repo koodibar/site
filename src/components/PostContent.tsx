26688da8638e0c8484a939626f38e5ea188bc90f
Hung Neo <hungnq1989@gmail.com>
2018-12-05 02:48:09 +0200

    Change font-size to 2rem

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -12,7 +12,7 @@ export const PostFullContent = styled.section`
   padding: 70px 100px 0;
   min-height: 230px;
   font-family: ${config.fontFamily};
-  font-size: 2.2rem;
+  font-size: 2rem;
   line-height: 1.6em;
   background: #fff;
 
